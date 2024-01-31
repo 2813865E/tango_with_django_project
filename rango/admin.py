5e57dbaae66089c0aae8174a85cfb18a29735509
“Dafydd <2813865E@student.gla.ac.uk>
2024-01-31 11:30:23 +0000

    Chapter 5 exercises completed

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from rango.models import Category, Page
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+#cass CategoryAdmin(admin.ModelAdmin):
+ #   list_display = ('name','views','likes')
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(Category)
