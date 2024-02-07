cedc67f0bb0e5b27d8318d3c8d2bcb1956eb5ec9
“Dafydd <2813865E@student.gla.ac.uk>
2024-02-07 21:00:22 +0000

    chapter 4 passed tests after doing chapter 10

--- rango/urls.py
+++ rango/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'rango'
 
@@ -17,4 +19,4 @@ urlpatterns = [
         path('logout/', views.user_logout, name='logout'),
 
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
